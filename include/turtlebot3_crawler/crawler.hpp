74bd2ebe91de54ac59bb862272aca57741e9a0ae
Shivang Patel <shivaang14@gmail.com>
2019-10-08 13:52:44 -0400

    Crawler functionality added
    - Also added launch file.

--- include/turtlebot3_crawler/crawler.hpp
+++ include/turtlebot3_crawler/crawler.hpp
@@ -4,12 +4,19 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
 
+#include <vector>
+
 class Crawler:public rclcpp::Node{
 public:
     Crawler();
+    ~Crawler();
 
 private:
-    void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+    void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) const;
+    int isNearObstacle(const std::vector<float> &) const;
+    void publishStop() const;
+    void publishGoStraight() const;
+    void publishTurn() const;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr sub_;
 };
--- src/crawler.cpp
+++ src/crawler.cpp
@@ -5,6 +5,8 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
 
+#include <vector>
+
 Crawler::Crawler():Node("Crawler"){
     pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
         "cmd_vel",
@@ -16,8 +18,72 @@ Crawler::Crawler():Node("Crawler"){
         rclcpp::SensorDataQoS(),
         std::bind(&Crawler::laserCallback, this, std::placeholders::_1)
     );
+
+    publishStop();
 }
 
-void Crawler::laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+Crawler::~Crawler(){
+
+    publishStop();
+}
+
+void Crawler::laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+const {
     RCLCPP_INFO(this->get_logger(), "I AM BEING CALLED");
+
+    if(isNearObstacle(msg->ranges)){
+        publishTurn();
+    } else {
+        publishGoStraight();
+    }
+}
+
+int Crawler::isNearObstacle(const std::vector<float> &ranges) const {
+    for(int i=315; i<360; i++){
+        if(ranges[i] < 0.45){
+            return 1;
+        }
+    }
+    for(int i=0; i<45; i++){
+        if(ranges[i] < 0.45){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void Crawler::publishStop() const{
+    auto twist_trans = geometry_msgs::msg::Twist();
+    twist_trans.linear.x = 0;
+    twist_trans.linear.y = 0;
+    twist_trans.linear.z = 0;
+    twist_trans.angular.x = 0;
+    twist_trans.angular.y = 0;
+    twist_trans.angular.z = 0;
+
+    pub_->publish(twist_trans);
+}
+
+void Crawler::publishGoStraight() const{
+    auto twist_trans = geometry_msgs::msg::Twist();
+    twist_trans.linear.x = 0.2;
+    twist_trans.linear.y = 0;
+    twist_trans.linear.z = 0;
+    twist_trans.angular.x = 0;
+    twist_trans.angular.y = 0;
+    twist_trans.angular.z = 0;
+
+    pub_->publish(twist_trans);
+}
+
+void Crawler::publishTurn() const{
+    auto twist_trans = geometry_msgs::msg::Twist();
+    twist_trans.linear.x = 0;
+    twist_trans.linear.y = 0;
+    twist_trans.linear.z = 0;
+    twist_trans.angular.x = 0;
+    twist_trans.angular.y = 0;
+    twist_trans.angular.z = 0.1;
+
+    pub_->publish(twist_trans);
 }
